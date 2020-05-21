9c8e170f04a0b0ce24bed369bf2fbfd0b30f5149
samerlagha <samer_la@mail.ru>
2020-05-22 01:21:20 +0300

    commit

--- Новая папка/mediumclone_hooks-21-improving-topbar/src/index.js
+++ Новая папка/mediumclone_hooks-21-improving-topbar/src/index.js
@@ -5,14 +5,17 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Routes from 'routes'
 import Topbar from 'components/topbar'
 import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
   return (
     <CurrentUserProvider>
-      <Router>
-        <Topbar />
-        <Routes />
-      </Router>
+      <CurrentUserChecker>
+        <Router>
+          <Topbar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
     </CurrentUserProvider>
   )
 }
